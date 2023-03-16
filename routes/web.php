16b76899712da5489765943a0e35b69614d0cb10
Dmitry Novik <exfriend@live.ru>
2023-03-16 09:21:16 +0200

    corrected namespace for web course controller (main page)

--- routes/web.php
+++ routes/web.php
@@ -2,4 +2,4 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [\App\Modules\Enrollment\Application\Controller\Web\CourseController::class, 'list']);
+Route::get('/', [\App\Modules\Course\Application\Controller\Web\CourseController::class, 'list']);
