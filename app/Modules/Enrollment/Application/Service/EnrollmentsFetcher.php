842b71d11fb5b53f08fb383e2b2ff706affef9df
Dmitry Novik <exfriend@live.ru>
2023-03-16 17:45:32 +0200

    sorting, filtering implemented, according template and vue changes done

--- app/Modules/Enrollment/Application/Service/EnrollmentsFetcher.php
+++ app/Modules/Enrollment/Application/Service/EnrollmentsFetcher.php
@@ -15,37 +15,48 @@ class EnrollmentsFetcher
         return $query;
     }
 
-    private function applySorting(Builder $query, Request $request)
+    private function applySorting(Builder $query, Request $request): void
     {
-        if ($request->has('sort_by')) {
+        if ($request->has('sort_by') && $request->get('sort_by') !== null) {
             $sortField = $request->get('sort_by');
-            $sortOrder = $request->get('sort_order', 'asc');
-            $query->orderBy($sortField, $sortOrder);
+
+            if ($sortField === 'alphabetical') {
+                $this->applyAlphabeticalOrdering($query);
+                return;
+            }
+
+            $sortField = $request->get('sort_by');
+            $query->orderBy($sortField);
         }
+    }
+
+    private function applyAlphabeticalOrdering(Builder $query): void
+    {
+        $query->select('enrollments.*')
+            ->join('courses', 'enrollments.course_id', '=', 'courses.id')
+            ->join('students', 'enrollments.student_id', '=', 'students.id')
+            ->orderBy('courses.title')
+            ->orderBy('students.name');
     }
 
     private function applyFilters(Builder $query, Request $request)
     {
-        // Filtering by course title
-        if ($request->has('course_title')) {
-            $query->where('title', 'LIKE', '%' . $request->get('course_title') . '%');
-        }
-
-        // Filtering by user name or email
-        if ($request->has('user_search')) {
-            $userSearch = $request->get('user_search');
-            $query->whereHas('users', function ($q) use ($userSearch) {
-                $q->where('name', 'LIKE', '%' . $userSearch . '%')
-                    ->orWhere('email', 'LIKE', '%' . $userSearch . '%');
+        if ($request->has('courseTitle') && $request->get('courseTitle') !== null) {
+            $titleSearch = $request->get('courseTitle');
+            $query->whereHas('course', function ($q) use ($titleSearch) {
+                $q->where('title', 'LIKE', '%' . $titleSearch . '%');
             });
         }
 
-        // Filtering by enrollment status
-        if ($request->has('status')) {
-            $query->whereHas('enrollments', function ($q) use ($request) {
-                $q->where('status', $request->get('status'));
+        if ($request->has('studentName') && $request->get('studentName') !== null) {
+            $studentSearch = $request->get('studentName');
+            $query->whereHas('student', function ($q) use ($studentSearch) {
+                $q->where('name', 'LIKE', '%' . $studentSearch . '%');
             });
         }
 
+        if ($request->has('status') && $request->get('status') !== null) {
+            $query->where('status', (int)$request->get('status'));
+        }
     }
 }
--- resources/js/Enrollments/EnrollmentsTable.vue
+++ resources/js/Enrollments/EnrollmentsTable.vue
@@ -1,8 +1,46 @@
 <template>
     <div class="container">
-        <table class="table">
+        <table class="table table-bordered">
             <thead>
             <tr>
+                <th>
+                    <div>
+                        <label for="sort">Сортировка:</label>
+                        <select class="form-select" id="sort" v-model="sort" @change="fetchData">
+                            <option value="updated_at">По дате завершения</option>
+                            <option value="created_at">По дате записи</option>
+                            <option value="alphabetical">По алфавиту</option>
+                        </select>
+                    </div>
+                </th>
+                <th>
+                    <div>
+                        <label for="courseFilter">Фильтр по курсу:</label>
+                        <input id="courseFilter" v-model="courseFilter" @input="fetchData" />
+                    </div>
+                </th>
+                <th>
+                    <div>
+                        <label for="studentFilter">Фильтр по пользователю:</label>
+                        <input id="studentFilter" v-model="studentFilter" @input="fetchData" />
+                    </div>
+                </th>
+                <th>
+                    <div>
+                        <label for="statusFilter">Фильтр по статусу:</label>
+                        <select id="statusFilter" v-model="statusFilter" @change="fetchData">
+                            <option value="">Все</option>
+                            <option value="0">In progress</option>
+                            <option value="1">Completed</option>
+                        </select>
+                    </div>
+                </th>
+                <th colspan="2">
+                    <button @click="fetchData" class="btn btn-primary">Обновить данные</button>
+                </th>
+            </tr>
+            <tr>
+                <th></th>
                 <th>Название курса</th>
                 <th>Имя пользователя</th>
                 <th>Результат</th>
@@ -12,6 +50,7 @@
             </thead>
             <tbody>
                 <tr v-for="enrollment in enrollments" :key="enrollment.id">
+                    <td></td>
                     <td>{{ enrollment.course_title }}</td>
                     <td>{{ enrollment.student_name }}</td>
                     <td><span :class="{ 'text-warning': enrollment.status == 'In Progress', 'text-success': enrollment.status == 'Completed' }">{{ enrollment.status }}</span></td>
@@ -20,7 +59,6 @@
                 </tr>
             </tbody>
         </table>
-        <button @click="fetchData">Обновить данные</button>
     </div>
 </template>
 
@@ -29,6 +67,10 @@ export default {
     data() {
         return {
             enrollments: [],
+            sort: 'updated_at',
+            courseFilter: '',
+            studentFilter: '',
+            statusFilter: '',
         };
     },
     created() {
@@ -37,7 +79,14 @@ export default {
     methods: {
         async fetchData() {
             try {
-                const response = await fetch('/api/enrollments');
+                const params = new URLSearchParams({
+                    sort_by: this.sort,
+                    courseTitle: this.courseFilter,
+                    studentName: this.studentFilter,
+                    status: this.statusFilter,
+                });
+
+                const response = await fetch(`/api/enrollments?${params}`);
                 const data = await response.json();
                 this.enrollments = await data.enrollments;
             } catch (error) {
@@ -49,5 +98,7 @@ export default {
 </script>
 
 <style scoped>
-
+    table {
+        margin-top: 50px;
+    }
 </style>
